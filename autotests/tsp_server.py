2bd71c2a039f5cc3ad378e034a35408178832d5a
Vlad Minashkin <minashkin@git.com>
2026-05-15 22:51:42 +0300

    реализация tcp сервера, с запоминанием сообщений

--- autotests/tsp_server.py
+++ autotests/tsp_server.py
@@ -8,19 +8,19 @@ def server():
     server_socket.bind(server_address)
 
     # Начинаем слушать входящие подключения (максимум 5 в очереди)
-    server_socket.listen(5)
+    server_socket.listen(10)
     print("Сервер запущен и ждет подключений на localhost:12345")
-
+    messages = [];
     while True:
         # Принимаем соединение от клиента
         client_socket, client_address = server_socket.accept()
         print(f"Подключение от {client_address}")
         # Получаем данные от клиента
         data = client_socket.recv(1024).decode()
-        print(f"Получено сообщение: {data}")
+        print(f"Получено сообщение: {data} от пользователя с адресом: {client_address}")
         # Отправляем ответ клиенту
-        response = f"Сервер получил: {data}"
-        client_socket.send(response.encode())
+        messages.append(data)
+        client_socket.send('\n'.join(messages).encode())
         # Закрываем соединение с клиентом
         client_socket.close()
 
