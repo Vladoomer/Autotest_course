cd201f7066c81e479aa5d2d9b67bcf4236bf61b6
Vlad Minashkin <minashkin@git.com>
2026-05-16 10:29:23 +0300

    домашнее задание по httpx

--- autotests/httpx_authentication.py
+++ autotests/httpx_authentication.py
@@ -10,10 +10,11 @@ login_response_data = login_response.json()
 print("Login response :",login_response_data)
 print("Login status code:", login_response.status_code)
 
-refresh_token_payload = {
-    "refreshToken": login_response_data['token']['refreshToken']
+access_token_headers = {
+    "Authorization": f"Bearer {login_response_data['token']['accessToken']}"
 }
-refresh_response = httpx.post("http://localhost:8000/api/v1/authentication/refresh", json=refresh_token_payload)
-refresh_response_data = refresh_response.json()
-print("Refresh response :",refresh_response_data)
-print("Refresh status code:", refresh_response.status_code)
+
+get_me_response = httpx.get("http://localhost:8000/api/v1/users/me", headers=access_token_headers)
+get_me_response_data = get_me_response.json()
+print(get_me_response_data)
+print(get_me_response.status_code)
