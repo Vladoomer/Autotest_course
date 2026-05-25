cc0bd533b0072ef82f776925bd233ecdfea607a9
Vlad Minashkin <minashkin@git.com>
2026-05-25 18:26:04 +0300

    добавление Docstring

--- autotests/clients/courses/courses_client.py
+++ autotests/clients/courses/courses_client.py
@@ -44,13 +44,39 @@ class CourseClient(APIClient):
         return self.get("/api/v1/courses", params=query)
 
     def get_course_api(self, course_id: str) -> Response:
+        """
+               Метод получения курса.
+
+               :param course_id: Идентификатор курса.
+               :return: Ответ от сервера в виде объекта httpx.Response
+               """
         return self.get(f"/api/v1/courses/{course_id}")
 
     def create_course_api(self, request: CreateCourseRequestDict) -> Response:
+        """
+                Метод создания курса.
+
+                :param request: Словарь с title, maxScore, minScore, description, estimatedTime,
+                previewFileId, createdByUserId.
+                :return: Ответ от сервера в виде объекта httpx.Response
+                """
         return self.post("/api/v1/courses", json=request)
 
     def update_course_api(self, exercise_id: str, request: UpdateCourseRequestDict) -> Response:
+        """
+                Метод обновления курса.
+
+                :param course_id: Идентификатор курса.
+                :param request: Словарь с title, maxScore, minScore, description, estimatedTime.
+                :return: Ответ от сервера в виде объекта httpx.Response
+                """
         return self.patch(f"/api/v1/courses/{exercise_id}", json=request)
 
     def delete_course_api(self, exercise_id: str) -> Response:
+        """
+                Метод удаления курса.
+
+                :param course_id: Идентификатор курса.
+                :return: Ответ от сервера в виде объекта httpx.Response
+                """
         return self.delete(f"/api/v1/courses/{exercise_id}")
