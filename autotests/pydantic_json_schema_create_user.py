95399646051c47309d3d04dc45f672eb485f06ed
Vlad Minashkin <minashkin@git.com>
2026-06-07 13:38:36 +0300

    Изучение валиадции с помощью jsonschema

---
+++ autotests/pydantic_json_schema_create_user.py
@@ -0,0 +1,23 @@
+from clients.users.public_users_client import get_public_users_client, CreateUserRequestSchema, CreateUserResponseSchema
+from faker import Faker
+from tools.assertions.schema import validate_json_schema
+from jsonschema import validate
+public_users_client = get_public_users_client()
+
+fake = Faker()
+create_user_request = CreateUserRequestSchema(
+    email=str(fake.email()),
+    password="string",
+    last_name="string",
+    first_name="string",
+    middle_name="string"
+)
+
+create_user_response = public_users_client.create_user_api(create_user_request)
+create_user_response_json = create_user_response.json()
+create_user_response_schema = CreateUserResponseSchema.model_json_schema()
+
+validate_json_schema(create_user_response_json, create_user_response_schema)
+
+
+
