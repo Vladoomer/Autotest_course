d2a86a3e9557e81ab9d345f2ea31391c63dbf886
Vlad Minashkin <minashkin@git.com>
2026-06-29 12:24:24 +0300

    исправление негативных тестов

--- autotests/tools/assertions/files.py
+++ autotests/tools/assertions/files.py
@@ -109,9 +109,9 @@ def assert_get_file_with_incorrect_file_id_response(actual: ValidationErrorRespo
             ValidationErrorSchema(
                 type= 'uuid_parsing',
                 location=['path', 'file_id'],
-                message="Input should be a valid UUID, invalid character: found `i` at 1",
+                message="Input should be a valid UUID, invalid character: expected an optional prefix of `urn:uuid:` followed by [0-9a-fA-F-], found `i` at 1",
                 input="incorrect-file-id",
-                context={"error": "invalid character: found `i` at 1"}
+                context={"invalid character: expected an optional prefix of `urn:uuid:` followed by [0-9a-fA-F-], found `i` at 1"}
             )]
     )
     assert_validation_error_response(actual, expected)
